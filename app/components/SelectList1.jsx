859a70632c40ad22ca24309b1edba9b86e0eb33d
yachao.ni <yachao.ni@ipinyou.com>
2017-02-27 09:43:08 +0800

    新增

---
+++ app/components/SelectList1.jsx
@@ -0,0 +1,95 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { connect } from 'react-redux';
+import React,{ Component } from 'react';
+import {Responsive, WidthProvider} from 'react-grid-layout';
+import { Router, Route, IndexRoute, Link, IndexLink } from 'react-router'
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import {List, ListItem} from 'material-ui/List';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import Avatar from 'material-ui/Avatar';
+import {pinkA200, transparent} from 'material-ui/styles/colors';
+import { withRouter } from 'react-router';
+import '../css/selectList.scss';
+
+{/* 第三 */}
+class MyActionGrade extends Component {
+  
+  constructor(props, context){
+    super(props, context);
+    this.color = transparent;
+  }
+
+  componentWillReceiveProps(nextProps){
+    if (this.props.selectedWords !== nextProps.selectedWords){
+      let index = nextProps.selectedWords.indexOf(this.props.item)
+      if(index > -1){
+        this.color = pinkA200;
+      } else {
+        this.color = transparent;
+      }
+    }
+  }
+
+  render(){
+    return(
+      <ActionGrade color={this.color} />
+    )
+
+  }
+}
+
+class SelectList1 extends Component {
+
+  constructor(props, context){
+    super(props, context);
+  }
+
+  static defaultProps = {
+    itemArray: {"": [
+      "空",
+    ]},
+
+  }
+
+  _clickItem(item) {
+
+  }
+
+  generateListDom(){
+
+    let listDomArray = []
+
+    for(let [category, wordList] of Object.entries(this.props.itemArray)){
+
+      for (let word of wordList){
+        listDomArray.push(
+          <ListItem
+          primaryText={word}
+          secondaryText={category}
+          onClick={() => this.props.handleClick(word)} />)
+          listDomArray.push(<Divider inset={true} />)
+        }
+    }
+    return listDomArray
+  }
+
+  render(){
+    return (
+        <List style={{'width': '100%','height': '620'}}>
+          {
+            this.generateListDom()
+          }
+          </List>
+    )
+  }
+
+}
+
+export default withRouter(SelectList1)
---
+++ app/components/tabs.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import SelectList1 from './SelectList1';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
+
+export default class TabsControlled extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
+  render() {
+    return (
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="近义词" value="a" >
+          <div style={styles.headline}>
+             <SelectList1
+               itemArray={this.props.wordList}
+               handleClick={this.props.insertTextToTempEditor} />
+         </div>
+          {/*<div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>*/}
+        </Tab>
+        <Tab label="行业词" value="b">
+          <div style={styles.headline}>
+             <SelectList1
+               itemArray={this.props.wordList}
+               handleClick={this.props.insertTextToTempEditor} />
+         </div>
+        </Tab>
+        <Tab label="高频词" value="c">
+          <div style={styles.headline}>
+             <SelectList1
+               itemArray={this.props.wordList}
+               handleClick={this.props.insertTextToTempEditor} />
+         </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}
